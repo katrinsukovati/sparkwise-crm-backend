a3895d7c00a80fcf3b0e45fa7dc43dfaa88d9cf1
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-21 22:36:32 -0500

    reverting back

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
+// Import dotenv to process environment variables from `.env` file.
 import "dotenv/config";
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 export default {
-  client: "pg",
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    charset: "utf8",
   },
 };
