5ea5f87cc725df3dca874a9dda8cc04ab41b3e9f
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-22 20:01:55 -0500

    using environment variables for google calendar to get events

--- controllers/calendar-controller.js
+++ controllers/calendar-controller.js
@@ -6,7 +6,21 @@ import fs from "fs";
 const { GOOGLE_CALENDAR_EMAIL } = process.env;
 
 // Load the service account credentials
-const credentials = JSON.parse(fs.readFileSync("./service-account-key.json"));
+// const credentials = JSON.parse(fs.readFileSync("./service-account-key.json"));
+
+const credentials = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
 
 // Authenticate using the service account
 const auth = new google.auth.GoogleAuth({
